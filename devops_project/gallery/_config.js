5df9e1e2465aac683418143fea757a03b06ed0d8
Timothy-creator400 <136724131+Timothy-creator400@users.noreply.github.com>
2023-06-24 21:36:07 +0300

    Add files via upload

---
+++ devops_project/gallery/_config.js
@@ -0,0 +1,9 @@
+var config = {}
+
+// Update to have your correct username and password
+config.mongoURI = {
+    production: "mongodb+srv://TimothyOdero:aRJYeJyM2@cluster1.uamac7h.mongodb.net/?retryWrites=true&w=majority",
+    development: "mongodb+srv://TimothyOdero:aRJYeJyM2@cluster1.uamac7h.mongodb.net/?retryWrites=true&w=majority",
+    test: "mongodb+srv://TimothyOdero:aRJYeJyM2@cluster1.uamac7h.mongodb.net/?retryWrites=true&w=majority",
+}
+module.exports = config;
